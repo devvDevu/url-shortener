fb8f3bf02e9e58775ebb4e3c07625db71350cf75
devvDevu <dev_devu@rambler.ru>
2025-03-02 10:53:49 -0300

    feat(dto.go): added description Added short description to swago(swagger doc generator)

--- internal/data_transfer_object/url_dto/request/request_url_post/dto.go
+++ internal/data_transfer_object/url_dto/request/request_url_post/dto.go
@@ -2,6 +2,11 @@ package request_url_post
 
 import "url-shortener/internal/common/types/url_types"
 
+// @Description DTO для создания короткой ссылки
 type UrlPostDto struct {
+	// Оригинальный URL для сокращения
+	// Required: true
+	// Example: https://example.com/path?param=value
+	// Format: url
 	OriginalUrl url_types.UrlOriginal `json:"original_url" validate:"required,url"`
 }
